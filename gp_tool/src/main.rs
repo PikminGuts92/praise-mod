a79f942f26ca798ed130acfbc90b9029ce026244
PikminGuts92 <PikminGuts92@gmail.com>
2020-12-14 17:21:37 -0500

    Add logging

--- gp_tool/src/main.rs
+++ gp_tool/src/main.rs
@@ -1,7 +1,15 @@
 mod apps;
 use apps::GPTool;
+use simplelog::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Setup logging
+    CombinedLogger::init(
+        vec![
+            TermLogger::new(LevelFilter::Info, Config::default(), TerminalMode::Mixed),
+        ]
+    )?;
+
     let mut scene = GPTool::new();
     scene.run()
 }
