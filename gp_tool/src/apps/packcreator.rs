776d1b7ce189868fa96fe877af654813d3728d8a
PikminGuts92 <PikminGuts92@gmail.com>
2021-01-01 00:26:41 -0500

    Refine chart to xml conversion and create directories for song pack

--- gp_tool/src/apps/packcreator.rs
+++ gp_tool/src/apps/packcreator.rs
@@ -7,38 +7,125 @@ use praise_mod_lib::pack::*;
 use praise_mod_lib::song::*;
 use praise_mod_lib::xml::*;
 use std::error::Error;
+use std::fs::create_dir_all;
 use std::path::{Path, PathBuf};
 
 #[derive(Clap, Debug)]
 pub struct PackCreatorApp {
     #[clap(about = "Path to input CH songs directory", required = true)]
     pub songs_path: String,
-    #[clap(about = "Path to output pack directory", required = true)]
+    #[clap(about = "Path to output song pack directory", required = true)]
     pub output_path: String,
+    #[clap(long, short, about = "Name of song pack")]
+    pub name: Option<String>,
+    #[clap(long, short, default_value = "4", about = "Numeric id for song pack (must be between 4-98")]
+    pub id: u8,
 }
 
 impl SubApp for PackCreatorApp {
     fn process(&mut self) -> Result<(), Box<dyn Error>> {
         let song_paths = find_dirs_with_file_name(&self.songs_path, "song.ini")?;
 
+        let pack_name = match &self.name {
+            Some(n) => n,
+            None => "Custom Song Pack",
+        };
+
+        let pack_id = self.id;
+        let mut song_id = 0u16;
+        let output_dir = Path::new(&self.output_path);
+
         // Iterate over song directories
         for path in &song_paths {
-            info!("Parsing song in \"{}\"", path.to_str().unwrap());
+            let song_meta = convert_song(path, pack_id, song_id, output_dir);
 
-            let song_ini = path.join("song.ini");
-            let song_meta = SongMeta::from_path(&song_ini)?;
+            if song_meta.is_err() {
+                warn!("Error parsing song, skipping");
+                continue;
+            }
 
-            info!("Song Information\n\tTitle: {}\n\tArtist: {}\n\tAlbum: {}\n\tYear: {}",
-                song_meta.name,
-                song_meta.artist,
-                song_meta.album,
-                song_meta.year,
-            );
+            let song_meta = song_meta?;
 
-            let song_chart_path = path.join("notes.chart");
-            let song_chart = SongChart::from_path(&song_chart_path)?;
-            let xml_file = XmlFile::from_chart(&song_chart, XmlTrackType::Guitar, Some(XmlTrackDifficulty::Expert));
+            song_id += 1; // Increment song id
         }
         Ok(())
     }
+}
+
+fn convert_song(path: &Path, pack_id: u8, song_id: u16, output_dir: &Path) -> Result<SongMeta, Box<dyn Error>> {
+    info!("Parsing song in \"{}\"", path.to_str().unwrap());
+
+    let song_ini = path.join("song.ini");
+    let song_meta = SongMeta::from_path(&song_ini)?;
+
+    info!("Song Information\n\tTitle: {}\n\tArtist: {}\n\tAlbum: {}\n\tYear: {}",
+        song_meta.name,
+        song_meta.artist,
+        song_meta.album,
+        song_meta.year,
+    );
+
+    let song_chart_path = path.join("notes.chart");
+    let song_chart = SongChart::from_path(&song_chart_path)?;
+
+    let instruments = [
+        XmlTrackType::Guitar,
+        XmlTrackType::Bass,
+        XmlTrackType::Vocals,
+    ];
+
+    let gtr_difficulties = [
+        XmlTrackDifficulty::Easy,
+        XmlTrackDifficulty::Medium,
+        XmlTrackDifficulty::Hard,
+        XmlTrackDifficulty::Expert,
+    ];
+
+    let full_song_id = format!("{:02}{:03}", pack_id, song_id);
+
+    let output_dir = output_dir
+        .join(&format!("{:02}", pack_id))
+        .join(&format!("{:03}", song_id));
+    
+    if !output_dir.exists() {
+        // Create directory
+        create_dir_all(&output_dir)?;
+    }
+
+    for ins_type in &instruments {
+        // Parse vocals track
+        if *ins_type == XmlTrackType::Vocals {
+            let xml_writer = XmlFile::from_chart(&song_chart, *ins_type, None);
+
+            let track_name = format!(
+                "GPL{}.xml",
+                full_song_id,
+            );
+
+            let xml_path = output_dir.join(track_name);
+            xml_writer.write_to_file(&xml_path)?;
+            continue;
+        }
+
+        // Parse guitar/bass tracks
+        for (i, diff) in gtr_difficulties.iter().enumerate() {
+            let xml_writer = XmlFile::from_chart(&song_chart, *ins_type, Some(*diff));
+
+            let track_name = format!(
+                "GP{}{}_{}.xml",
+                match &ins_type {
+                    XmlTrackType::Guitar => "G",
+                    XmlTrackType::Bass => "B",
+                    XmlTrackType::Vocals => "L",
+                },
+                &full_song_id,
+                i,
+            );
+
+            let xml_path = output_dir.join(track_name);
+            xml_writer.write_to_file(&xml_path)?;
+        }
+    }
+
+    Ok(song_meta)
 }
