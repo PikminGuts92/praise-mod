973e56691e8bbeaae7947b40a32d73b3d1bd5ab6
PikminGuts92 <PikminGuts92@gmail.com>
2021-01-21 01:01:05 -0500

    Filter logging

--- gp_tool/src/main.rs
+++ gp_tool/src/main.rs
@@ -3,10 +3,15 @@ use apps::GPTool;
 use simplelog::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let log_config = ConfigBuilder::new()
+        .add_filter_allow_str("gp_tool")
+        .add_filter_allow_str("praise_mod_lib")
+        .build();
+
     // Setup logging
     CombinedLogger::init(
         vec![
-            TermLogger::new(LevelFilter::Info, Config::default(), TerminalMode::Mixed),
+            TermLogger::new(LevelFilter::Info, log_config, TerminalMode::Mixed),
         ]
     )?;
 
