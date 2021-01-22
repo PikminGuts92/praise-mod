cc9123eac0ccccd8ab552c3ec2a2e933d86e4d3b
PikminGuts92 <PikminGuts92@gmail.com>
2021-01-21 21:50:02 -0500

    Update rust-ini and remove custom utf8 bom parsing

--- praise_mod_lib/src/song/song_meta.rs
+++ praise_mod_lib/src/song/song_meta.rs
@@ -15,15 +15,8 @@ pub struct SongMeta {
 
 impl SongMeta {
     pub fn from_path(ini_path: &Path) -> Result<SongMeta, Box<dyn Error>> {
-        // Read file to string
-        // Note: This is a workaround until rust-ini is updated to support UTF-8 BOM #79
-        let mut file_str = String::new();
-        File::open(ini_path)?.read_to_string(&mut file_str)?;
-        let ini_str = skip_bom(&file_str);
-
         // TODO: Throw error if ini not found or "song" section not present
-        //let song_ini = Ini::load_from_file(ini_path)?;
-        let song_ini = Ini::load_from_str(&ini_str)?;
+        let song_ini = Ini::load_from_file(ini_path)?;
 
         // Can be either "song" or "Song"
         let song_section_name = song_ini
@@ -59,14 +52,5 @@ impl SongMeta {
                 None => 2020,
             },
         })
-    }
-}
-
-fn skip_bom(s: &str) -> &str {
-    // Skip BOM if present
-    if s.starts_with("\u{feff}") {
-        &s[3..]
-    } else {
-        s
     }
 }
