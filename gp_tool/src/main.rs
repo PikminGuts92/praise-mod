5d8bff1cf7d82ff78bbe8c9dc0907b291be6c5eb
PikminGuts92 <PikminGuts92@gmail.com>
2021-01-23 18:57:22 -0500

    Update logging to use debug level for debug builds

--- gp_tool/src/main.rs
+++ gp_tool/src/main.rs
@@ -2,6 +2,12 @@ mod apps;
 use apps::GPTool;
 use simplelog::*;
 
+#[cfg(debug_assertions)]
+const LOG_LEVEL: LevelFilter = LevelFilter::Debug;
+
+#[cfg(not(debug_assertions))]
+const LOG_LEVEL: LevelFilter = LevelFilter::Info;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let log_config = ConfigBuilder::new()
         .add_filter_allow_str("gp_tool")
@@ -11,7 +17,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Setup logging
     CombinedLogger::init(
         vec![
-            TermLogger::new(LevelFilter::Info, log_config, TerminalMode::Mixed),
+            TermLogger::new(LOG_LEVEL, log_config, TerminalMode::Mixed),
         ]
     )?;
 
