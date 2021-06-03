069efb0c71c1a448e72fcc63a57d7b99aeb1a668
PikminGuts92 <PikminGuts92@gmail.com>
2021-06-03 00:29:12 -0400

    Remove pre-encoded silent audio track

--- praise_mod_lib/src/audio/mod.rs
+++ praise_mod_lib/src/audio/mod.rs
@@ -13,8 +13,6 @@ use std::error::Error;
 use std::fs::{copy, create_dir_all, read, write};
 use std::path::{Path, PathBuf};
 
-const SILENT_DPO: &'static [u8] = include_bytes!("silence.dpo");
-
 pub fn ogg_to_dpo(in_path: &Path, out_path: &Path) -> Result<(), Box<dyn Error>> {
     // Read in bytes
     let mut data = read(&in_path)?;
@@ -26,12 +24,6 @@ pub fn ogg_to_dpo(in_path: &Path, out_path: &Path) -> Result<(), Box<dyn Error>>
 
     // Write to file
     write(&out_path, data)?;
-    Ok(())
-}
-
-pub fn save_dpo_slience(out_path: &Path) -> Result<(), Box<dyn Error>> {
-    // Write to file (already "encrypted")
-    write(&out_path, &SILENT_DPO)?;
     Ok(())
 }
 
