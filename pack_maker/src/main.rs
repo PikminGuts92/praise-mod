6ad7ab70ce1aa8099a265a3be495c5fb698bfb70
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2022-02-02 23:53:07 -0500

    Add icon for pack maker

--- pack_maker/src/main.rs
+++ pack_maker/src/main.rs
@@ -3,13 +3,36 @@
 
 mod app;
 use app::*;
+use eframe::{NativeOptions, run_native};
+use eframe::epi::IconData;
+use image::{load_from_memory, EncodableLayout};
+
+static ICON: &'static [u8] = include_bytes!("../res/icon.png");
 
 fn main() {
     let app = PackApp::default();
-    let ops = eframe::NativeOptions {
+    let icon = transform_image_to_icon(ICON);
+
+    let ops = NativeOptions {
         drag_and_drop_support: true,
-        ..eframe::NativeOptions::default()
+        icon_data: Some(icon),
+        ..NativeOptions::default()
     };
 
-    eframe::run_native(Box::new(app), ops);
+    run_native(Box::new(app), ops);
+}
+
+fn transform_image_to_icon(data: &[u8]) -> IconData {
+    // Open image
+    let img = load_from_memory(data).unwrap();
+
+    // Convert to rgba
+    let rgba = img.as_rgba8().unwrap();
+    let rgba_data = rgba.as_bytes();
+
+    IconData {
+        rgba: rgba_data.into(),
+        width: img.width(),
+        height: img.height(),
+    }
 }
